ccac1aafb4d3ed01975b72506ed2e9d8dcf04f10
matsuhisa <matsuhisa+github@gmail.com>
2021-03-20 23:55:19 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require(`apollo-server`)
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({url}) => console.log(`GraphQL Server running on ${url}`))
