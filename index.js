04595b4ac8559f54c519041865d38677bf5cbb07
matsuhisa <matsuhisa+github@gmail.com>
2021-03-21 14:30:40 +0900

    update mutation

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const { ApolloServer } = require(`apollo-server`)
-
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
   }
 `
 
+let _id = 0
+let photos = []
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
-  }
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+
+  Mutation: {
+    postPhoto( parent, args ) {
+      let newPhoto = {
+        id: _id++,
+        ...args
+      }
+      photos.push(newPhoto)
+      return newPhoto
+    }
+  },
+  // Photo: {
+  //     url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+  // }
 }
 
 const server = new ApolloServer({
