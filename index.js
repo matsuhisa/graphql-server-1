f2dd55b16398c8f33d4afb295e40aefb100bf601
matsuhisa <matsuhisa+github@gmail.com>
2021-03-22 00:29:08 +0900

    Add PhotoCategory, User

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const { ApolloServer } = require(`apollo-server`)
 const typeDefs = `
+  enum PhotoCategory {
+    SELFIE
+    PORTTRIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
+    description: String
+    category: PhotoCategory!
+    githubUser: String
+    postedBy: User!
+  }
+
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTTRIT
     description: String
   }
 
@@ -13,12 +37,31 @@ const typeDefs = `
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Photo!
+    postPhoto(input: PostPhotoInput): Photo!
   }
 `
 
-let _id = 0
-let photos = []
+let _id = 2
+const users = [
+  { 'githubLogin': 'foo', 'name': 'たんじろう' },
+  { 'githubLogin': 'bar', 'name': 'ねづこ' },
+]
+let photos = [
+  {
+    'id': 1,
+    'name': 'Dropping the Heart Chute 1',
+    'description': 'あいうえお',
+    'category': 'ACTION',
+    'githubUser': 'foo'
+  },
+  {
+    'id': 2,
+    'name': 'Dropping the Heart Chute 2',
+    'description': 'かきくけこ',
+    'category': 'ACTION',
+    'githubUser': 'bar'
+  },
+]
 
 const resolvers = {
   Query: {
@@ -30,7 +73,7 @@ const resolvers = {
     postPhoto( parent, args ) {
       let newPhoto = {
         id: _id++,
-        ...args
+        ...args.input
       }
       photos.push(newPhoto)
       return newPhoto
@@ -38,7 +81,20 @@ const resolvers = {
   },
   // ルートに追加されたリゾルバ => トリビアリゾルバ
   Photo: {
-      url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      console.log("----------")
+      users.forEach(user => console.log(user.githubLogin))
+      console.log(parent.githubUser)
+      console.log(users.filter( u => u.githubLogin === parent.githubUser))
+      console.log("----------")
+      return users.find( u => u.githubLogin === parent.githubUser)
+    }
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter( p => p.githubUser === parent.githubLogin )
+    }
   }
 }
 
