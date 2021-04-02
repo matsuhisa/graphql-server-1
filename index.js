39a2fb91361d0923ef030c28d9243735755488a9
matsuhisa <matsuhisa+github@gmail.com>
2021-04-03 00:39:17 +0900

    Use mongodb

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
 const { readFileSync } = require('fs')
+const { MongoClient } = require('mongodb')
+
+require('dotenv').config()
 
 const typeDefs = readFileSync('./typeDefs.graphql', 'utf-8')
 const resolvers = require('./resolvers')
@@ -52,13 +55,41 @@ let tags = [
   { 'PhotoID': '2', 'UserId': 'bar' },
 ]
 
-let app = express()
-const server = new ApolloServer({ typeDefs, resolvers })
-server.applyMiddleware({ app })
+// 1. 非同期関数を用意する
+const start = async () => {
+  const app = express()
+  const MONGO_DB = process.env.DB_HOST
 
-app.get('/', (req, res) => res.end(`Welocome to PhotoShare API`))
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+  console.log('-----------')
+  console.log(process.env.DB_HOST)
+  console.log('-----------')
+  let db
 
-app.listen({ port: 4000 }, () => {
-  console.log(`GraphQL Server running @  http://localhost:4000${server.graphqlPath}`)
-})
+  try {
+    const client = await MongoClient.connect(MONGO_DB, { useUnifiedTopology: true, useNewUrlParser: true })
+    db = client.db()
+    console.log('connect!')
+  } catch (error) {
+    console.log(`
+    
+      Mongo DB Host not found!
+      please add DB_HOST environment variable to .env file
+      exiting...
+       
+    `)
+    process.exit(1)
+  }
+
+  const context = { db }
+  const server = new ApolloServer({ typeDefs, resolvers, context })
+  server.applyMiddleware({ app })
+  
+  app.get('/', (req, res) => res.end(`Welocome to PhotoShare API`))
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+  
+  app.listen({ port: 4000 }, () => {
+    console.log(`GraphQL Server running @  http://localhost:4000${server.graphqlPath}`)
+  })
+}
+
+start()
