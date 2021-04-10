c969be3b18ebc4b58ac52dc50ce0d4638b9dd29e
matsuhisa <matsuhisa+github@gmail.com>
2021-04-10 16:03:50 +0900

    コードの整理

--- index.js
+++ index.js
@@ -9,73 +9,21 @@ require('dotenv').config()
 const typeDefs = readFileSync('./typeDefs.graphql', 'utf-8')
 const resolvers = require('./resolvers')
 
-let _id = 2
-const users = [
-  { 'githubLogin': 'foo', 'name': 'たんじろう' },
-  { 'githubLogin': 'bar', 'name': 'ねづこ' },
-  { 'githubLogin': 'kaminari', 'name': 'ぜんじろう' },
-]
-let photos = [
-  {
-    'id': 1,
-    'name': 'Dropping the Heart Chute 1',
-    'description': 'あいうえお',
-    'category': 'ACTION',
-    'githubUser': 'foo',
-    'created': '3-19-1998',
-  },
-  {
-    'id': 2,
-    'name': 'Dropping the Heart Chute 2',
-    'description': 'かきくけこ',
-    'category': 'ACTION',
-    'githubUser': 'bar',
-    'created': '3-19-2008',
-  },
-  {
-    'id': 3,
-    'name': 'Dropping the Heart Chute 3',
-    'description': 'さしすせそ',
-    'category': 'ACTION',
-    'githubUser': 'kaminari',
-    'created': '3-19-2018',
-  },
-  {
-    'id': 4,
-    'name': 'Dropping the Heart Chute 4',
-    'description': 'たちつてと',
-    'category': 'ACTION',
-    'githubUser': 'kaminari',
-    'created': '3-19-2028',
-  },
-]
-let tags = [
-  { 'PhotoID': '1', 'UserId': 'foo' },
-  { 'PhotoID': '2', 'UserId': 'foo' },
-  { 'PhotoID': '2', 'UserId': 'bar' },
-]
-
 // 1. 非同期関数を用意する
 const start = async () => {
   const app = express()
   const MONGO_DB = process.env.DB_HOST
 
-  console.log('-----------')
-  console.log(process.env.DB_HOST)
-  console.log('-----------')
   let db
-
   try {
     const client = await MongoClient.connect(MONGO_DB, { useUnifiedTopology: true, useNewUrlParser: true })
     db = client.db()
-    console.log('connect!')
+    console.log('db connect!')
   } catch (error) {
     console.log(`
-    
       Mongo DB Host not found!
       please add DB_HOST environment variable to .env file
       exiting...
-       
     `)
     process.exit(1)
   }
