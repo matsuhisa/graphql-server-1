286131a26cc351ad7c017c65ef7ed3412dda31d1
matsuhisa <matsuhisa+github@gmail.com>
2021-04-10 16:04:44 +0900

    resolvers の書き換え

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,5 +1,18 @@
 module.exports = {
-  totalPhotos: () => photos.length,
-  allPhotos: () => photos,
-  allUsers: () => users
+  // 写真をの数を返す
+  totalPhotos: (parent, args, { db }) =>
+    db.collection(`photos`).estimatedDocumentCount(),
+
+  // すべての写真を返す
+  allPhotos: (parent, args, { db }) => 
+    db.collection(`photos`)
+    .find().toArray(),
+  
+  totalUsers: (parent, args, { db }) => 
+    db.collection(`users`).estimatedDocumentCount(),
+  
+  allUsers: (parent, args, { db }) => 
+    db.collection(`users`)
+    .find().toArray()
+
 }
