58300f53f3bb1f0170d2e6302dae475d68e6c190
matsuhisa <matsuhisa+github@gmail.com>
2021-04-10 17:19:55 +0900

    Add authorizeWithGithub for Mutation.js

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const { authorizeWithGithub } = require("../lib")
+
 module.exports = {
   postPhoto( parent, args ) {
     let newPhoto = {
@@ -7,5 +9,35 @@ module.exports = {
     }
     photos.push(newPhoto)
     return newPhoto
+  },
+
+  async authorizeWithGithub( parent, {code}, {db} ) {
+    let {
+      message, access_token, avater_url, lgoin, name
+    } = await authorizeWithGithub({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+
+    // 2. メッセージがあれば何かしらのエラーが発生している
+    if(message) {
+      throw new Error(message)
+    }
+
+    // 3. データーを一つのオブジェクトにまとめる
+    let latestuserInfo = {
+      name,
+      githubLogin: lgoin,
+      githubToken: access_token,
+      avater: avater_url,
+    }
+
+    // 4. 新しい情報もとにレコードを追加したり更新したりする
+    const { ops:[user] } = await db
+      .collection(`users`)
+      .replaceOne({ githubLogin: login }, latestuserInfo, {upsert: true})
+
+    return { user, token: access_token }
   }
 }
